2f64f20b8c6a86393d8cfb22313719a5e112daf3
Gabriel Klein <gabriel.klein@philips.com>
2024-07-29 16:11:06 -0300

    Adicionado layout da tela de login

--- stock-control/src/app/app.module.ts
+++ stock-control/src/app/app.module.ts
@@ -6,14 +6,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { HomeComponent } from './modules/home/home.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    // PrimeNg
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
