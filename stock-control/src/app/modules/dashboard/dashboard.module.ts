32c0dcf851a34b1d616f76d0305de7d6fc33660e
Gabriel Klein <gabriel.klein@philips.com>
2024-08-04 12:58:59 -0300

    Added toolbar, chart and shared module

--- stock-control/src/app/modules/dashboard/dashboard.module.ts
+++ stock-control/src/app/modules/dashboard/dashboard.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
-
-import { DashboardHomeComponent } from './page/dashboard-home/dashboard-home.component';
-import { DASHBOARD_ROUTES } from './dashboard.routing';
+import { ChartModule } from 'primeng/chart';
 import { CardModule } from 'primeng/card';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
-import { CookieService } from 'ngx-cookie-service';
+
+import { DashboardHomeComponent } from './page/dashboard-home/dashboard-home.component';
+import { DASHBOARD_ROUTES } from './dashboard.routing';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [DashboardHomeComponent],
@@ -27,6 +29,8 @@ import { CookieService } from 'ngx-cookie-service';
     ToolbarModule,
     CardModule,
     ToastModule,
+    ChartModule,
+    SharedModule,
   ],
   providers: [MessageService, CookieService],
 })
